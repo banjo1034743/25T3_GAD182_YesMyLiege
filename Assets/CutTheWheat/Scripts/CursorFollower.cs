d3217b920b327450cd578ee9b8720d15c701ee51
Banjo - CAD Laptop <1034743@student.sae.edu.au>
2025-11-01 20:36:26 +1100

    Updated CursorFollower
    Added methods and variables to the script. Commiting to make change to Input Action Asset to return Vector3 rather than Vector2

--- Assets/CutTheWheat/Scripts/CursorFollower.cs
+++ Assets/CutTheWheat/Scripts/CursorFollower.cs
@@ -1,7 +1,43 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+/// <summary>
+/// This class is what is responsible for making objects follow the cursor
+/// </summary>
 public class CursorFollower : MonoBehaviour
 {
+    #region Variables
+
+    [Header("Objects")]
+
+    [Tooltip("Place the object you want to follow the cursor here")]
+    [SerializeField] private GameObject _objectFollowingCursor;
+
+    [Header("Components")]
+
+    [Tooltip("Place the camera used in the scene here.")]
+    // Used for getting the mouses position usiing ScreenToWorldPosition
+    [SerializeField] private Camera _camera;
+
+    #endregion
+
+    #region Methods
+
+    private Vector3 GetMousePosition()
+    {
+        return _camera.ScreenToWorldPoint(Mouse.current.position);
+    }
+
+    private void SetObjectToMouse()
+    {
+
+    }
+
+    #endregion
+
+    #region Unity Methods
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +49,6 @@ public class CursorFollower : MonoBehaviour
     {
         
     }
+
+    #endregion
 }
