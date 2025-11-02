c239e6767aa848c6791ecb236db67971e0a6d2aa
Banjo - CAD Laptop <1034743@student.sae.edu.au>
2025-11-02 15:39:44 +1100

    Code Functionality (broken) and Scythe Game Object added
    Updated script with methods with functionality. Current script does not move the scythe game object with the mouse. I'll be pushing this now to go into main to edit the InputActionAsset once more

--- Assets/CutTheWheat/Scripts/CursorFollower.cs
+++ Assets/CutTheWheat/Scripts/CursorFollower.cs
@@ -1,6 +1,6 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 /// <summary>
 /// This class is what is responsible for making objects follow the cursor
@@ -20,18 +20,47 @@ public class CursorFollower : MonoBehaviour
     // Used for getting the mouses position usiing ScreenToWorldPosition
     [SerializeField] private Camera _camera;
 
+    [Header("Input")]
+
+    [Tooltip("Place the action map with your input actions for the microgame here.")]
+    // We reference this to use for ensuring we get the correct action
+    // when initializing _moveAction.
+    private InputActionMap _cutTheWheatActionMap;
+
+    [Tooltip("Place the input action for moving (usually mouse position) here")]
+    // The reference to the input action for moving the sycthe.
+    // We have a reference for reading the position of the mouse
+    // to apply to our object
+    private InputAction _moveAction;
+
     #endregion
 
     #region Methods
 
     private Vector3 GetMousePosition()
     {
-        return _camera.ScreenToWorldPoint(Mouse.current.position);
+        if (_moveAction != null)
+        {
+            //Debug.Log("_moveAction is not null!");
+            Debug.Log(_moveAction.activeControl);
+            Vector2 readValue = _camera.ScreenToWorldPoint(_moveAction.ReadValue<Vector2>());
+            return new Vector3(readValue.x, readValue.y, 10);
+        }
+        else
+        {
+            return Vector3.zero;
+        }
     }
 
     private void SetObjectToMouse()
     {
+        _objectFollowingCursor.transform.position = GetMousePosition();
+    }
 
+    private void InitializeInputActions()
+    {
+        _cutTheWheatActionMap = InputSystem.actions.FindActionMap("Microgame/CutTheWheat");
+        _moveAction = _cutTheWheatActionMap.FindAction("Move");
     }
 
     #endregion
@@ -41,13 +70,13 @@ public class CursorFollower : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InitializeInputActions();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetObjectToMouse();
     }
 
     #endregion
