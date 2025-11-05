0c0fda984149765b887ce68c4c050ef6ae99e29c
Banjo - CAD Laptop <1034743@student.sae.edu.au>
2025-11-05 22:30:25 +1100

    SettingsManager Gutted to Wanted Functionality
    Edited the imported SettingsManager script to only have functionality for adjusting master volume. The script was also added to a gameobject in a test scene, along with an audiomixer being made so it can be tested later on.

--- Assets/SettingsMenu/Scripts/SettingsManager.cs
+++ Assets/SettingsMenu/Scripts/SettingsManager.cs
@@ -2,81 +2,33 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
 
-public class Settings : PlayerPrefs
+namespace DirtPoorPeasants.Settings
 {
-    public float masterVolumeValue = PlayerPrefs.GetFloat("masterVolumeValue", 0);
-    public float musicVolumeValue = PlayerPrefs.GetFloat("musicVolumeValue", 0);
-    public float sfxVolumeValue = PlayerPrefs.GetFloat("sfxVolumeValue", 0);
-    public int graphicsValue = PlayerPrefs.GetInt("graphicsValue", 0);
-    public float textSpeed = PlayerPrefs.GetFloat("textSpeed", 0.03f);
-}
-
-public class SettingsManager : MonoBehaviour
-{
-    public Slider masterVolSlider;
-    public Slider musicVolSlider;
-    public Slider sfxVolSlider;
-    public AudioMixer mainAudioMixer;
-
-    public Button graphicButtonLow;
-    public Button graphicButtonModerate;
-    public Button graphicButtonHigh;
-    private int valueIntGraphics;
-
-    public Button textButtonSlow;
-    public Button textButtonMedium;
-    public Button textButtonFast;
-    [SerializeField] private float valueFloatText;
-
-    private string currentScene;
-
-    private void Awake()
+    public class Settings : PlayerPrefs
     {
-        masterVolSlider.value = PlayerPrefs.GetFloat("masterVolumeValue");
-        musicVolSlider.value = PlayerPrefs.GetFloat("musicVolumeValue");
-        sfxVolSlider.value = PlayerPrefs.GetFloat("sfxVolumeValue");
-
-        valueIntGraphics = PlayerPrefs.GetInt("graphicsValue");
-        QualitySettings.SetQualityLevel(valueIntGraphics);
-
-        valueFloatText = PlayerPrefs.GetFloat("textSpeed");
-        PlayerPrefs.SetFloat("textSpeed", valueFloatText);
+        public float masterVolumeValue = PlayerPrefs.GetFloat("masterVolumeValue", 0);
     }
 
-    public void ChangeMasterVolume()
+    public class SettingsManager : MonoBehaviour
     {
-        mainAudioMixer.SetFloat("masterVolume", masterVolSlider.value);
-    }
+        [SerializeField] private Slider masterVolSlider;
+        [SerializeField] private AudioMixer mainAudioMixer;
 
-    public void ChangeMusicVolume()
-    {
-        mainAudioMixer.SetFloat("musicVolume", musicVolSlider.value);
-    }
+        private string currentScene;
 
-    public void ChangeSFXVolume()
-    {
-        mainAudioMixer.SetFloat("sfxVolume", sfxVolSlider.value);
-    }
+        private void Awake()
+        {
+            masterVolSlider.value = PlayerPrefs.GetFloat("masterVolumeValue");
+        }
 
-    public void ChangeGraphicsQuality(int graphicsSetting)
-    {
-        valueIntGraphics = graphicsSetting;
+        public void ChangeMasterVolume()
+        {
+            mainAudioMixer.SetFloat("masterVolume", masterVolSlider.value);
+        }
 
-        QualitySettings.SetQualityLevel(valueIntGraphics);
-    }
-
-    public void ChangeTextSpeed(float speed)
-    {
-        valueFloatText = speed;
-        PlayerPrefs.SetFloat("textSpeed", valueFloatText);
-    }
-
-    public void SaveChanges()
-    {
-        PlayerPrefs.SetFloat("masterVolumeValue", masterVolSlider.value);
-        PlayerPrefs.SetFloat("musicVolumeValue", musicVolSlider.value);
-        PlayerPrefs.SetFloat("sfxVolumeValue", sfxVolSlider.value);
-        PlayerPrefs.SetInt("graphicsValue", valueIntGraphics);
-        PlayerPrefs.SetFloat("textSpeed", valueFloatText);
+        public void SaveChanges()
+        {
+            PlayerPrefs.SetFloat("masterVolumeValue", masterVolSlider.value);
+        }
     }
 }
