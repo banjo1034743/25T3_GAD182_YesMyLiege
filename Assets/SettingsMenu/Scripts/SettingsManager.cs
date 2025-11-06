b2df7337b14f4610c240a9871ff8248d51fef4d7
Banjo - CAD Laptop <1034743@student.sae.edu.au>
2025-11-07 03:48:56 +1100

    Settings Test Scene Functional
    Slider changes volume of master volume with help text displayed, and the exit button takes players out of the screen

--- Assets/SettingsMenu/Scripts/SettingsManager.cs
+++ Assets/SettingsMenu/Scripts/SettingsManager.cs
@@ -11,24 +11,52 @@ namespace DirtPoorPeasants.Settings
 
     public class SettingsManager : MonoBehaviour
     {
+        #region Variables
+
+        [SerializeField] private GameObject _settingsMenu;
+
+        [Header("Master Volume")]
+
         [SerializeField] private Slider masterVolSlider;
+
         [SerializeField] private AudioMixer mainAudioMixer;
 
-        private string currentScene;
+        #endregion
 
-        private void Awake()
-        {
-            masterVolSlider.value = PlayerPrefs.GetFloat("masterVolumeValue");
-        }
+        #region Methods
 
         public void ChangeMasterVolume()
         {
             mainAudioMixer.SetFloat("masterVolume", masterVolSlider.value);
         }
 
+        public void ToggleSettingsMenu()
+        {
+            switch (_settingsMenu.activeSelf)
+            {
+                case true:
+                    _settingsMenu.SetActive(false);
+                    break;
+                case false:
+                    _settingsMenu.SetActive(true);
+                    break;
+            }
+        }
+
         public void SaveChanges()
         {
             PlayerPrefs.SetFloat("masterVolumeValue", masterVolSlider.value);
         }
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+            masterVolSlider.value = PlayerPrefs.GetFloat("masterVolumeValue");
+        }
+
+        #endregion
     }
 }
