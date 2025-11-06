f8531577417fc02ceb9d3a16a8be6d93bedbe722
Banjo - CAD Laptop <1034743@student.sae.edu.au>
2025-11-06 17:38:35 +1100

    Functions and Variables for GroundCollider Added
    The script has also been added to the ground object.

---
+++ Assets/FeedMyAnimals/Scripts/GroundCollider.cs
@@ -0,0 +1,54 @@
+using System.Diagnostics.Contracts;
+using UnityEngine;
+
+namespace DirtPoorPeasants.FeedMyAnimals
+{
+    public class GroundCollider : MonoBehaviour
+    {
+        #region Variables
+
+        [SerializeField] private Collider thisCollider;
+
+        #endregion
+
+        #region Methods
+
+        public Vector3 GetGroundColliderBoundsSize()
+        {
+
+        }
+
+        public Vector3 GetGroundColliderBoundsCentre()
+        {
+
+        }
+
+        public Collider GetCollider()
+        {
+
+        }
+
+        private void DestroyApple(GameObject apple)
+        {
+
+        }
+
+        #endregion
+
+        #region Unity Methods
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        #endregion
+    }
+}
