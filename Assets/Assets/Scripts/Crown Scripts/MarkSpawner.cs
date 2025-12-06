aea7a39eb1540186e2861ab41683a78123adc110
PerhapsADev <leighton.stack.edu@gmail.com>
2025-12-06 14:39:37 +1100

    Exported PTC to Dev

--- Assets/Assets/Scripts/Crown Scripts/MarkSpawner.cs
+++ Assets/Assets/Scripts/Crown Scripts/MarkSpawner.cs
@@ -1,10 +1,10 @@
 
 using UnityEngine;
 
-public class DungSpawner : MonoBehaviour
+public class MarkSpawner : MonoBehaviour
 {
-    int DungPlacements;
-    public PrefabDungScript dung;
+    int MarkPlacements;
+    public PrefabMarkScript mark;
     // public int[] dungLocationSpaces; (Add for quaility of life to spawn dung off each other using array)
     private void Start()
     {
@@ -13,17 +13,17 @@ public class DungSpawner : MonoBehaviour
 
         while (currentCount < maxCount)
         {
-            Debug.Log("count for dung went up");
+            Debug.Log("count for Mark went up");
             currentCount++;
-            float x = Random.Range(9f, -9f);
-            Instantiate(dung, new Vector3(x, 0, 0), Quaternion.Euler (0,0,90));
-            Debug.Log("Spawned dung");
+            float x = Random.Range(0.21f, -0.18f);
+            float y = Random.Range(2.1f, 1.75f);
+            // Set area on crown to spawn Marks
+            Instantiate(mark, new Vector3(x, y, 1.950f), Quaternion.identity);
+            Debug.Log("Spawned Mark");
 
         }
         GameManager.Instance.objectiveCounter.SetCounter(currentCount);
         GameManager.Instance.objectiveCount = currentCount;
     }
 }
-
-// could change to track number of dung till it switches off instaead of times it commanded for simplicity?
 
